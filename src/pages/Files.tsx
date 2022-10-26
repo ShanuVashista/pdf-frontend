6ac4740c54ee5bcc6674c5a65877d98fcd6eb871
shailesh chaurasiya <shaileshc535@gmail.com>
2022-10-26 14:23:57 +0530

    update as per user

--- src/pages/Files.tsx
+++ src/pages/Files.tsx
@@ -1,196 +1,206 @@
-import React, {useEffect, useState} from "react";
-import {ReactStateDeclaration, UISref} from "@uirouter/react";
-import {Button, Divider, Grid, IconButton, Paper, TextField, Tooltip, Typography,} from "@material-ui/core";
-import {Edit, File, Share2, Trash, Upload} from "react-feather";
-import {FileType} from "../types";
-import {$crud} from "../factories/CrudFactory";
-import {ShareDialog} from "../Dialogs/ShareDialog";
-import {generateFormData} from "../helpers";
-import {EmptyContainer} from "./EmptyContainer";
-import {Loading} from "./Loading";
+import React, { useEffect, useState } from "react";
+import { ReactStateDeclaration, UISref } from "@uirouter/react";
+import {
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  Paper,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@material-ui/core";
+import { Edit, File, Share2, Trash, Upload } from "react-feather";
+import { FileType } from "../types";
+import { $crud } from "../factories/CrudFactory";
+import { ShareDialog } from "../Dialogs/ShareDialog";
+import { generateFormData } from "../helpers";
+import { EmptyContainer } from "./EmptyContainer";
+import { Loading } from "./Loading";
 
 export function Files() {
-    let [limit] = useState(10);
-    let [page, setPage] = useState(1);
-    const [files, setFiles] = useState<FileType[]>([]);
-    const [file, setFile] = useState<FileType>(null);
-    const [show, setShow] = useState(false);
-    const [loading, setLoading] = useState(false);
-    let [searchValue, setSearchValue] = useState<string>("");
+  let [limit] = useState(10);
+  let [page, setPage] = useState(1);
+  const [files, setFiles] = useState<FileType[]>([]);
+  const [file, setFile] = useState<FileType>(null);
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  let [searchValue, setSearchValue] = useState<string>("");
 
-    const retrieveFiles = async () => {
-        try {
-            setLoading(true);
-            const {data} = await $crud.post("file/list-files", {
-                limit,
-                page,
-                cond: {search: searchValue},
-            });
-            setFiles(data);
-        } finally {
-            setLoading(false);
-        }
-    };
+  const retrieveFiles = async () => {
+    try {
+      setLoading(true);
+      const { data } = await $crud.post("file/list-files", {
+        limit,
+        page,
+        cond: { search: searchValue },
+      });
+      setFiles(data);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const deleteFile = async (id) => {
-        await $crud.confirm({
-            textContent: "This file won't be revert.",
-        });
-        await $crud.put(`file/delete-file/${id}`);
-        retrieveFiles();
-    };
+  console.log("files", files);
 
-    const uploadFile = async () => {
-        setLoading(true);
-        try {
-            const file = await $crud.chooseFile({accept: "application/pdf"});
-            await $crud.post(
-                "file/add-file",
-                generateFormData({
-                    filename: file,
-                    docname: file.name,
-                })
-            );
-        } finally {
-            retrieveFiles();
-            setLoading(false);
-        }
-    };
+  const deleteFile = async (id) => {
+    await $crud.confirm({
+      textContent: "This file won't be revert.",
+    });
+    await $crud.put(`file/delete-file/${id}`);
+    retrieveFiles();
+  };
 
-    useEffect(() => {
-        retrieveFiles();
-    }, []);
+  const uploadFile = async () => {
+    setLoading(true);
+    try {
+      const file = await $crud.chooseFile({ accept: "application/pdf" });
+      await $crud.post(
+        "file/add-file",
+        generateFormData({
+          filename: file,
+          docname: file.name,
+        })
+      );
+    } finally {
+      retrieveFiles();
+      setLoading(false);
+    }
+  };
 
-    useEffect(() => {
-    }, []);
+  useEffect(() => {
+    retrieveFiles();
+  }, []);
 
-    const searchData = async (e) => {
-        searchValue = e.target.value;
+  useEffect(() => {}, []);
 
-        await setSearchValue(e.target.value);
+  const searchData = async (e) => {
+    searchValue = e.target.value;
 
-        if (searchValue !== "") {
-            retrieveFiles();
-        } else {
-            setSearchValue("");
-            retrieveFiles();
-        }
-    };
+    await setSearchValue(e.target.value);
 
-    return (
-        <Grid className="p-2 p-2-all">
-            <Grid container alignItems="center" className="p-2-all">
-                <Typography
-                    component={Grid}
-                    item
-                    xs
-                    variant="h5"
-                    className="font-weight-bold"
-                >
-                    PDF Files
-                </Typography>
+    if (searchValue !== "") {
+      retrieveFiles();
+    } else {
+      setSearchValue("");
+      retrieveFiles();
+    }
+  };
 
-                <Grid>
-                    <TextField
-                        fullWidth
-                        label="Search"
-                        value={searchValue}
-                        onChange={searchData}
-                        variant="outlined"
-                        size="small"
-                        color="primary"
-                    />
-                </Grid>
-                <Grid>
-                    <Button variant="outlined" onClick={uploadFile}>
-                        <Upload size={16} className="mr-2"/> Upload
-                    </Button>
-                </Grid>
-            </Grid>
-            {!loading ? (
-                files.length !== 0 ? (
-                    <Grid container alignItems="center" className="p-2-all">
-                        {files.map((file, index) => (
-                            <Grid item xs={6} md={4} lg={3} key={index}>
-                                <Paper elevation={1}>
-                                    <Grid container alignItems="center">
-                                        <Grid
-                                            item
-                                            xs={12}
-                                            className="p-3 text-center position-relative"
-                                        >
-                                            <File size={180} className="text-success"/>
-                                            <IconButton
-                                                style={{top: 12, right: 12}}
-                                                className="position-absolute"
-                                                onClick={() => {
-                                                    setShow(true);
-                                                    setFile(file);
-                                                }}
-                                            >
-                                                <Tooltip title="Share File For Signature">
-                                                    <Share2 size={20}/>
-                                                </Tooltip>
-                                            </IconButton>
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Divider/>
-                                        </Grid>
-                                        <Grid
-                                            item
-                                            xs={12}
-                                            container
-                                            alignItems="center"
-                                            className="p-2"
-                                        >
-                                            <Typography
-                                                component={Grid}
-                                                item
-                                                xs
-                                                variant="body2"
-                                                className="p-1 font-weight-bold"
-                                            >
-                                                {file.docname}
-                                            </Typography>
-                                            <UISref to="fileViewer" params={{fileId: file._id}}>
-                                                <IconButton>
-                                                    <Edit size={16}/>
-                                                </IconButton>
-                                            </UISref>
-                                            <IconButton onClick={() => deleteFile(file._id)}>
-                                                <Trash className="text-danger" size={16}/>
-                                            </IconButton>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                            </Grid>
-                        ))}
-                    </Grid>
-                ) : (
-                    <Grid className="p-md-5">
-                        <EmptyContainer content="You don't have any uploaded file"/>
-                    </Grid>
-                )
-            ) : (
-                <Loading/>
-            )}
-            <ShareDialog
-                fileId={file?._id}
-                open={show}
-                onClose={() => setShow(false)}
-            />
+  return (
+    <Grid className="p-2 p-2-all">
+      <Grid container alignItems="center" className="p-2-all">
+        <Typography
+          component={Grid}
+          item
+          xs
+          variant="h5"
+          className="font-weight-bold"
+        >
+          PDF Files
+        </Typography>
+
+        <Grid>
+          <TextField
+            fullWidth
+            label="Search"
+            value={searchValue}
+            onChange={searchData}
+            variant="outlined"
+            size="small"
+            color="primary"
+          />
         </Grid>
-    );
+        <Grid>
+          <Button variant="outlined" onClick={uploadFile}>
+            <Upload size={16} className="mr-2" /> Upload
+          </Button>
+        </Grid>
+      </Grid>
+      {!loading ? (
+        files.length !== 0 ? (
+          <Grid container alignItems="center" className="p-2-all">
+            {files.map((file, index) => (
+              <Grid item xs={6} md={4} lg={3} key={index}>
+                <Paper elevation={1}>
+                  <Grid container alignItems="center">
+                    <Grid
+                      item
+                      xs={12}
+                      className="p-3 text-center position-relative"
+                    >
+                      <File size={180} className="text-success" />
+                      <IconButton
+                        style={{ top: 12, right: 12 }}
+                        className="position-absolute"
+                        onClick={() => {
+                          setShow(true);
+                          setFile(file);
+                        }}
+                      >
+                        <Tooltip title="Share File For Signature">
+                          <Share2 size={20} />
+                        </Tooltip>
+                      </IconButton>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Divider />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      container
+                      alignItems="center"
+                      className="p-2"
+                    >
+                      <Typography
+                        component={Grid}
+                        item
+                        xs
+                        variant="body2"
+                        className="p-1 font-weight-bold"
+                      >
+                        {file.docname}
+                      </Typography>
+                      <UISref to="fileViewer" params={{ fileId: file._id }}>
+                        <IconButton>
+                          <Edit size={16} />
+                        </IconButton>
+                      </UISref>
+                      <IconButton onClick={() => deleteFile(file._id)}>
+                        <Trash className="text-danger" size={16} />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Grid className="p-md-5">
+            <EmptyContainer content="You don't have any uploaded file" />
+          </Grid>
+        )
+      ) : (
+        <Loading />
+      )}
+      <ShareDialog
+        fileId={file?._id}
+        open={show}
+        onClose={() => setShow(false)}
+      />
+    </Grid>
+  );
 }
 
 export const states: ReactStateDeclaration[] = [
-    {
-        url: "/files",
-        name: "files",
-        data: {
-            title: "Files",
-            loggedIn: true,
-        },
-        component: Files,
+  {
+    url: "/files",
+    name: "files",
+    data: {
+      title: "Files",
+      loggedIn: true,
     },
+    component: Files,
+  },
 ];
--- src/pages/SharedWithMe.tsx
+++ src/pages/SharedWithMe.tsx
@@ -1,89 +1,129 @@
-import React, {useEffect, useState} from "react";
-import {ReactStateDeclaration, UISref} from "@uirouter/react";
-import {Divider, Grid, IconButton, Paper, Typography} from "@material-ui/core";
-import {Edit, File} from "react-feather";
-import {SharedWithMeFileType} from "../types";
-import {$crud} from "../factories/CrudFactory";
-import {EmptyContainer} from "./EmptyContainer";
-import {Loading} from "./Loading";
+import React, { useEffect, useState } from "react";
+import { ReactStateDeclaration, UISref } from "@uirouter/react";
+import {
+  Divider,
+  Grid,
+  IconButton,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import { Edit, File } from "react-feather";
+import { SharedWithMeFileType } from "../types";
+import { $crud } from "../factories/CrudFactory";
+import { EmptyContainer } from "./EmptyContainer";
+import { Loading } from "./Loading";
 
 export function SharedWithMeFiles() {
-    const [files, setFiles] = useState<SharedWithMeFileType[]>([]);
-    const [loading, setLoading] = useState(false);
+  const [files, setFiles] = useState<SharedWithMeFileType[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    const retrieveFiles = async () => {
-        try {
-            setLoading(true);
-            const {data} = await $crud.post("files/receive-files-list", {
-                limit: "10",
-                page: "1"
-            });
-            setFiles(data);
-        } finally {
-            setLoading(false);
-        }
+  const retrieveFiles = async () => {
+    try {
+      setLoading(true);
+      const { data } = await $crud.post("files/receive-files-list", {
+        paginate: true,
+        limit: "10",
+        page: "1",
+      });
+      setFiles(data);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    };
+  const handleDocId = (id) => {
+    localStorage.setItem("fileDocId", id);
+  };
 
-    useEffect(() => {
-        retrieveFiles();
-    }, []);
+  useEffect(() => {
+    retrieveFiles();
+  }, []);
 
-    return <Grid className="p-2 p-2-all">
-        <Grid container alignItems="center" className="p-2-all">
-            <Typography component={Grid} item xs variant="h5" className="font-weight-bold">
-                Shared PDF Files With Me For Signature
-            </Typography>
-        </Grid>
-        {
-            !loading ? files.length !== 0 ?
-                <Grid container alignItems="center" className="p-2-all">
-                    {
-                        files.map((file, index) => <Grid item xs={6} md={4} lg={3} key={index}>
-                            <Paper elevation={1}>
-                                <Grid container alignItems="center">
-                                    <Grid item xs={12} className="p-3 text-center position-relative">
-                                        <File size={180} className="text-success"/>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Divider/>
-                                    </Grid>
-                                    <Grid item xs={12} container wrap="nowrap" alignItems="center" className="p-2">
-                                        <Grid container direction="column">
-                                            <Typography variant="body2" className="p-1">
-                                                {file.fileId.docname}
-                                            </Typography>
-                                            <Typography className="p-1 font-weight-bold text-primary"
-                                                        style={{fontSize: 11}}>
-                                                Shared By {file.senderId.fullname}
-                                            </Typography>
-                                        </Grid>
-                                        <UISref to="fileViewer" params={{fileId: file.fileId._id}}>
-                                            <IconButton>
-                                                <Edit size={16}/>
-                                            </IconButton>
-                                        </UISref>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
-                        </Grid>)
-                    }
-                </Grid>
-                : <Grid className="p-md-5">
-                    <EmptyContainer content="You don't have any shared file for signature"/>
-                </Grid> : <Loading/>
-        }
+  return (
+    <Grid className="p-2 p-2-all">
+      <Grid container alignItems="center" className="p-2-all">
+        <Typography
+          component={Grid}
+          item
+          xs
+          variant="h5"
+          className="font-weight-bold"
+        >
+          Shared PDF Files With Me For Signature
+        </Typography>
+      </Grid>
+      {!loading ? (
+        files.length !== 0 ? (
+          <Grid container alignItems="center" className="p-2-all">
+            {files.map((file, index) => (
+              <Grid item xs={6} md={4} lg={3} key={index}>
+                <Paper elevation={1}>
+                  <Grid container alignItems="center">
+                    <Grid
+                      item
+                      xs={12}
+                      className="p-3 text-center position-relative"
+                    >
+                      <File size={180} className="text-success" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Divider />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      container
+                      wrap="nowrap"
+                      alignItems="center"
+                      className="p-2"
+                    >
+                      <Grid container direction="column">
+                        <Typography variant="body2" className="p-1">
+                          {file.fileId.docname}
+                        </Typography>
+                        <Typography
+                          className="p-1 font-weight-bold text-primary"
+                          style={{ fontSize: 11 }}
+                        >
+                          Shared By {file.senderId.fullname}
+                        </Typography>
+                      </Grid>
+                      <UISref
+                        to="fileViewer"
+                        params={{
+                          fileId: file.fileId._id,
+                        }}
+                      >
+                        <IconButton onClick={() => handleDocId(file._id)}>
+                          <Edit size={16} />
+                        </IconButton>
+                      </UISref>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Grid className="p-md-5">
+            <EmptyContainer content="You don't have any shared file for signature" />
+          </Grid>
+        )
+      ) : (
+        <Loading />
+      )}
     </Grid>
+  );
 }
 
 export const states: ReactStateDeclaration[] = [
-    {
-        url: "/shared-with-me-files",
-        name: "sharedWithMeFiles",
-        data: {
-            title: "Shared With Me Files",
-            loggedIn: true
-        },
-        component: SharedWithMeFiles
-    }
+  {
+    url: "/shared-with-me-files",
+    name: "sharedWithMeFiles",
+    data: {
+      title: "Shared With Me Files",
+      loggedIn: true,
+    },
+    component: SharedWithMeFiles,
+  },
 ];
