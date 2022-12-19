0d0f7151b3a57cfc8225996c70534d9e89554af9
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2022-12-19 18:21:22 +0530

    Bug Fixed

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,239 +1,240 @@
-import { ReactStateDeclaration, UISref } from "@uirouter/react";
-import React, { useState } from "react";
-import {
-  Button,
-  Grid,
-  IconButton,
-  TextField,
-  Typography,
-} from "@material-ui/core";
-import { Eye, EyeOff } from "react-feather";
-import { $crud } from "../factories/CrudFactory";
-import { $state } from "../router";
-import { Loading } from "./Loading";
+import {ReactStateDeclaration, UISref} from "@uirouter/react";
+import React, {useEffect, useState} from "react";
+import {Button, Grid, IconButton, TextField, Typography,} from "@material-ui/core";
+import {Eye, EyeOff} from "react-feather";
+import {$crud} from "../factories/CrudFactory";
+import {Loading} from "./Loading";
 import PasswordChecklist from "react-password-checklist";
+import {$state} from "../router";
 
 export function SignUpPage() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [disabled, setDisabled] = useState(true);
-  const [error, setError] = useState(null);
-  const [params, setParams] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    phone: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const setParam = (name: string, value: any) => {
-    setParams((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState(null);
+    const [params, setParams] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: "",
+        password: "",
+        confirmPassword: "",
     });
-  };
 
-  function isValidEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-  }
+    const setParam = (name: string, value: any) => {
+        setParams((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    };
 
-  const handleChange = (e) => {
-    setParam("email", e.target.value);
-    if (!isValidEmail(e.target.value)) {
-      setError("Email is invalid");
-    } else {
-      setError(null);
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
     }
-  };
 
-  const setDisabledFunc = (val) => {
-    if (val == true) {
-      if (
-        params.firstname != null &&
-        params.phone != null &&
-        params.email != null
-      ) {
-        setDisabled(false);
-      }
-    } else {
-      setDisabled(true);
-    }
-  };
+    const handleChange = (e) => {
+        setParam("email", e.target.value);
+        if (!isValidEmail(e.target.value)) {
+            setError("Email is invalid");
+        } else {
+            setError(null);
+        }
+    };
 
-  const register = async () => {
-    try {
-      setLoading(true);
-      await $crud.post("user/register", params);
-    } finally {
-      await setLoading(false);
-      $state.go("login");
-    }
-  };
+    const setDisabledFunc = (val) => {
+        if (val == true) {
+            if (
+                params.firstname != null &&
+                params.phone != null &&
+                params.email != null
+            ) {
+                setDisabled(false);
+            }
+        } else {
+            setDisabled(true);
+        }
+    };
 
-  return (
-    <Grid
-      item
-      xs
-      container
-      alignItems="center"
-      justifyContent="center"
-      className="p-2 p-2-all bg-white"
-    >
-      {loading && <Loading />}
-      <Grid item xs={12} sm={6} lg={4}>
+    const register = async () => {
+        try {
+            setLoading(true);
+            const d = await $crud.post("user/register", params);
+            setStatus(d.status);
+        } catch (e) {
+            $crud.notify({type: 'error', message: e.message})
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (status === 200) $state.go('login')
+    }, [status]);
+
+    return (
         <Grid
-          container
-          direction="column"
-          wrap="nowrap"
-          className="p-2 p-2-all"
+            item
+            xs
+            container
+            alignItems="center"
+            justifyContent="center"
+            className="p-2 p-2-all bg-white"
         >
-          <Typography
-            component={Grid}
-            variant="h4"
-            className="mb-3 font-weight-bold"
-          >
-            PDF Sign Register
-          </Typography>
-          <Grid container className="p-2-all">
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                size="small"
-                variant="outlined"
-                label="First Name"
-                value={params.firstname}
-                onChange={(e) => setParam("firstname", e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                size="small"
-                variant="outlined"
-                label="Last Name"
-                value={params.lastname}
-                onChange={(e) => setParam("lastname", e.target.value)}
-              />
-            </Grid>
-          </Grid>
-          <Grid>
-            <TextField
-              fullWidth
-              type="email"
-              size="small"
-              variant="outlined"
-              label="Email"
-              value={params.email}
-              onChange={(e) => handleChange(e)}
-            />
-            {error && (
-              <Typography variant="subtitle2" style={{ color: "red" }}>
-                {error}
-              </Typography>
-            )}
-          </Grid>
-          <Grid>
-            <TextField
-              fullWidth
-              size="small"
-              variant="outlined"
-              label="Phone"
-              value={params.phone}
-              onChange={(e) => setParam("phone", e.target.value)}
-            />
-          </Grid>
-          <Grid>
-            <TextField
-              fullWidth
-              size="small"
-              type={showPassword ? "text" : "password"}
-              variant="outlined"
-              label="Password"
-              value={params.password}
-              onChange={(e) => setParam("password", e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <IconButton onClick={() => setShowPassword(!showPassword)}>
-                    {!showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
-                  </IconButton>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid>
-            <TextField
-              fullWidth
-              size="small"
-              type={showConfirmPassword ? "text" : "password"}
-              variant="outlined"
-              label="Confirm Password"
-              value={params.confirmPassword}
-              onChange={(e) => setParam("confirmPassword", e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  >
-                    {!showConfirmPassword ? (
-                      <Eye size={18} />
-                    ) : (
-                      <EyeOff size={18} />
-                    )}
-                  </IconButton>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid>
-            <PasswordChecklist
-              rules={["minLength", "specialChar", "number", "capital", "match"]}
-              minLength={8}
-              value={params.password}
-              valueAgain={params.confirmPassword}
-              onChange={(isValid) => setDisabledFunc(isValid)}
-            />
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item xs>
-              <Typography style={{ fontSize: 12 }} className="text-uppercase">
-                You already have a account?
-                <UISref to="login">
+            {loading && <Loading/>}
+            <Grid item xs={12} sm={6} lg={4}>
+                <Grid
+                    container
+                    direction="column"
+                    wrap="nowrap"
+                    className="p-2 p-2-all"
+                >
+                    <Typography
+                        component={Grid}
+                        variant="h4"
+                        className="mb-3 font-weight-bold"
+                    >
+                        PDF Sign Register
+                    </Typography>
+                    <Grid container className="p-2-all">
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                size="small"
+                                variant="outlined"
+                                label="First Name"
+                                value={params.firstname}
+                                onChange={(e) => setParam("firstname", e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                size="small"
+                                variant="outlined"
+                                label="Last Name"
+                                value={params.lastname}
+                                onChange={(e) => setParam("lastname", e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid>
+                        <TextField
+                            fullWidth
+                            type="email"
+                            size="small"
+                            variant="outlined"
+                            label="Email"
+                            value={params.email}
+                            onChange={(e) => handleChange(e)}
+                        />
+                        {error && (
+                            <Typography variant="subtitle2" style={{color: "red"}}>
+                                {error}
+                            </Typography>
+                        )}
+                    </Grid>
+                    <Grid>
+                        <TextField
+                            fullWidth
+                            size="small"
+                            variant="outlined"
+                            label="Phone"
+                            value={params.phone}
+                            onChange={(e) => setParam("phone", e.target.value)}
+                        />
+                    </Grid>
+                    <Grid>
+                        <TextField
+                            fullWidth
+                            size="small"
+                            type={showPassword ? "text" : "password"}
+                            variant="outlined"
+                            label="Password"
+                            value={params.password}
+                            onChange={(e) => setParam("password", e.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                        {!showPassword ? <Eye size={18}/> : <EyeOff size={18}/>}
+                                    </IconButton>
+                                ),
+                            }}
+                        />
+                    </Grid>
+                    <Grid>
+                        <TextField
+                            fullWidth
+                            size="small"
+                            type={showConfirmPassword ? "text" : "password"}
+                            variant="outlined"
+                            label="Confirm Password"
+                            value={params.confirmPassword}
+                            onChange={(e) => setParam("confirmPassword", e.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <IconButton
+                                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                    >
+                                        {!showConfirmPassword ? (
+                                            <Eye size={18}/>
+                                        ) : (
+                                            <EyeOff size={18}/>
+                                        )}
+                                    </IconButton>
+                                ),
+                            }}
+                        />
+                    </Grid>
+                    <Grid>
+                        <PasswordChecklist
+                            rules={["minLength", "specialChar", "number", "capital", "match"]}
+                            minLength={8}
+                            value={params.password}
+                            valueAgain={params.confirmPassword}
+                            onChange={(isValid) => setDisabledFunc(isValid)}
+                        />
+                    </Grid>
+                    <Grid container alignItems="center">
+                        <Grid item xs>
+                            <Typography style={{fontSize: 12}} className="text-uppercase">
+                                You already have a account?
+                                <UISref to="login">
                   <span className="d-inline-block ml-1 pointer text-primary">
                     Login
                   </span>
-                </UISref>
-              </Typography>
+                                </UISref>
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Button
+                                disabled={disabled}
+                                color="primary"
+                                size="small"
+                                variant="contained"
+                                onClick={register}
+                            >
+                                Register
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid>
-              <Button
-                disabled={disabled}
-                color="primary"
-                size="small"
-                variant="contained"
-                onClick={register}
-              >
-                Register
-              </Button>
-            </Grid>
-          </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-  );
+    );
 }
 
 export const states: ReactStateDeclaration[] = [
-  {
-    url: "/sign-up",
-    name: "signupUser",
-    data: {
-      title: "Register User",
-      loggedOut: true,
+    {
+        url: "/sign-up",
+        name: "signupUser",
+        data: {
+            title: "Register User",
+            loggedOut: true,
+        },
+        component: SignUpPage,
     },
-    component: SignUpPage,
-  },
 ];
